ef81572f72259b6647ba7ed64f78edc33dc8762f
frank_spr <frank_spr@163.com>
2017-04-28 15:54:46 +0800

    fix bug

--- tulongmap.py
+++ tulongmap.py
@@ -17,100 +17,131 @@ def main():
       RequestUrl = words[6]
       #define vars
       ClientIP = words[0]
-      FromAPI = "/api/"
-      FromYY = "/yy/login"
+      FromAPI = "/api/login.php"
+      FromYY = "/yy/"
       Frompps = "/pps/"
-      From37 = "/37/"
-      Fromsogou = "/sogou/"
-      From4399 = "/4399/login"
-      Fromxunlei = "/xunlei/"
+      From37 = "/37/login.php"
+#      Fromsogou = "/sogou/"
+      From4399 = "/4399/login.php"
+      Fromxunlei = "/xunlei/login.php"
+      #define platform uid vars
+      apiUid = 'uid'
+      yyUid = 'account'
+      ppsUid = 'user_id'
+      sqUid = 'user_name'
+      ssjjUid = 'username'
+      xunleiUid = 'username'
       #Catch 
       if re.match(r"%s" %FromAPI,RequestUrl):
         try:
-          list = re.split('&',RequestUrl)
-          if len(list) >=6:
+          lst = re.split('&',RequestUrl)
+          if len(lst) >=6:
             try:
-              uid = list[0]
-              platform = list[1]
-              UID = (re.split('=',uid))[1]
-              Platform = (re.split('=',platform))[1]
-	      print '%s\t%s\t%s\t%s' % (ClientIP ,UID , Platform)
+              for k in lst:
+                if re.search(r'%s' % apiUid,k):
+                  UID = (re.split('=',k))[1]
+                if  re.search(r'platform',k):
+                  Platform = (re.split('=',k))[1]
+              print '%s\t%s\t%s' % (ClientIP ,UID , Platform)
 	    except:
 	      pass
         except:
           pass 
       if re.match(r"%s" % FromYY,RequestUrl):
         try:
-          list = re.split('&',RequestUrl)
-          if len(list) >=6:
+          lst = re.split('&',RequestUrl)
+          if len(lst) >=6:
             try:
-              uid = list[0]
-              UID = (re.split('=',uid))[1]
-              Platform = 'yy'
-              print '%s\t%s\t%s' % (ClientIP ,UID , Platform)
+              for uid in lst:
+                if re.search(r'%s' % yyUid,uid):
+		  UID = (re.split('=',uid))[1] 
+                  Platform = 'yy'
+                  print '%s\t%s\t%s' % (ClientIP ,UID , Platform)
+                else:
+                  pass
             except:
               pass
         except:
 	  pass
       if re.match(r"%s" % Frompps,RequestUrl):
         try:
-          list = re.split('&',RequestUrl)
-	  if len(list) >=6:
+          lst = re.split('&',RequestUrl)
+	  if len(lst) >=6:
             try:
-              uid = list[0]
-              UID = (re.split('=',uid))[1]
-              Platform = 'pps'
-              print '%s\t%s\t%s' % (ClientIP ,UID , Platform)
+              for uid in lst:
+                if re.search(r'%s' % ppsUid,uid):
+                  UID = (re.split('=',uid))[1]
+                  Platform = 'pps'
+                  print '%s\t%s\t%s' % (ClientIP ,UID , Platform)
+                else:
+                  pass
+
+#              uid = lst[0]
+#              UID = (re.split('=',uid))[1]
+#              Platform = 'pps'
+#              print '%s\t%s\t%s' % (ClientIP ,UID , Platform)
 	    except:
 	      pass
         except:
           pass
       if re.match(r"%s" % From37,RequestUrl):
         try:
-          list = re.split('&',RequestUrl)
-          if len(list) >=6:
+          lst = re.split('&',RequestUrl)
+          if len(lst) >=6:
 	    try:
-              uid = list[0]
-              UID = (re.split('=',uid))[1]
-              Platform = '37'
-	      print '%s\t%s\t%s' % (ClientIP ,UID , Platform)
+              for uid in lst:
+                if re.search(r'%s' % sqUid,uid):
+                  UID = (re.split('=',uid))[1]
+                  Platform = '37'
+                  print '%s\t%s\t%s' % (ClientIP ,UID , Platform)
+                else:
+                  pass
+
+#              uid = lst[0]
+#              UID = (re.split('=',uid))[1]
+#              Platform = '37'
+#	      print '%s\t%s\t%s' % (ClientIP ,UID , Platform)
 	    except:
 	      pass
         except:
           pass
-      if re.match(r"%s" % Fromsogou,RequestUrl):
-        try:
-          list = re.split('&',RequestUrl)
-          if len(list) >= 7:
-            try:
-              uid = list[2]
-              UID = (re.split('=',uid))[1]
-              Platform = 'sogo'
-	      print '%s\t%s\t%s' % (ClientIP ,UID , Platform)
-            except:
-              pass
-        except:
-	  pass
+#      if re.match(r"%s" % Fromsogou,RequestUrl):
+#        try:
+#          lst = re.split('&',RequestUrl)
+#          if len(lst) >= 7:
+#            try:
+#              uid = lst[2]
+#              UID = (re.split('=',uid))[1]
+#              Platform = 'sogo'
+#	      print '%s\t%s\t%s' % (ClientIP ,UID , Platform)
+#            except:
+#              pass
+#        except:
+#	  pass
       if re.match(r"%s" % From4399,RequestUrl):
         try:
-          list = re.split('&',RequestUrl)
+          lst = re.split('&',RequestUrl)
 	  try:
-            uid = list[0]
-            UID = (re.split('=',uid))[1]
-            Platform = '4399'
-	    print '%s\t%s\t%s' % (ClientIP ,UID , Platform)
+              for uid in lst:
+                if re.search(r'%s' % ssjjUid,uid):
+                  UID = (re.split('=',uid))[1]
+                  Platform = '4399'
+                  print '%s\t%s\t%s' % (ClientIP ,UID , Platform)
           except:
 	    pass
         except:
 	  pass
       if re.match(r"%s" % Fromxunlei,RequestUrl):
         try:
-          list = re.split('&',RequestUrl)
+          lst = re.split('&',RequestUrl)
 	  try:
-            uid = list[0]
-            UID = (re.split('=',uid))[1]
-            Platform = 'xunlei'
-            print '%s\t%s\t%s' % (ClientIP ,UID , Platform)
+              for uid in lst:
+                if re.search(r'%s' % xunleiUid,uid):
+                  UID = (re.split('=',uid))[1]
+                  Platform = 'xunlei'
+                  print '%s\t%s\t%s' % (ClientIP ,UID , Platform)
+                else:
+                  pass
           except:
 	    pass
         except:
